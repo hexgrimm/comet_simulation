234515e4024ee3642e5dbdbf42cb85cfea1cdec1
HexGrimm <hexgrim@gmail.com>
2014-12-01 23:32:21 +0300

    Линзы, камера фолов, материалы, маппинг

---
+++ Unity_Project/Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MainCameraController : MonoBehaviour
+{
+
+    public GameObject Rosetta;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.LookAt(Rosetta.transform);
+        transform.RotateAround(Rosetta.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
